3d7f636cef2c0fb2cd06ebf6547f7c43f8cf3e2d
BuildTools <unconfigured@null.spigotmc.org>
2021-11-21 20:48:00 +0900

    1.2.0 버전입니다! 플레이어에게 책을 보여주는 메서드와 책을 커스텀으로 생성할 수 있는 메서드를 개발했습니다.

--- src/main/java/me/else_junsuk/junseclibrary/playerapi/P.java
+++ src/main/java/me/else_junsuk/junseclibrary/playerapi/P.java
@@ -1,7 +1,6 @@
 package me.else_junsuk.junseclibrary.playerapi;
 
 import me.else_junsuk.junseclibrary.itemutilapi.CustomItem;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -11,17 +10,13 @@ public class P {
      * 책을 직접 생성하고 생성 후 바로 플레이어에게 보여줍니다.
      * @since 1.2.0
      * @param player
-     * @param bookitemstack
      * @param title
      * @param author
      * @param content
      */
-    public static void openCreateBook(Player player, ItemStack bookitemstack, String title, String author, String... content) {
-        if (bookitemstack.getType() == Material.WRITTEN_BOOK) {
-            ItemStack book = CustomItem.createBook(title, author, content);
-            player.openBook(book);
-        }
-        else return;
+    public static void openCreateBook(Player player, String title, String author, String... content) {
+        ItemStack book = CustomItem.createBook(title, author, content);
+        player.openBook(book);
     }
 
 }
