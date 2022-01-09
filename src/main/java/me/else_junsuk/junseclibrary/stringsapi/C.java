45fd8b20b5f4cbf54ab5975c1410cf35a4eb9cd6
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 18:02:42 +0900

    새로운 C클래스를 생성했습니다. 이제 이 클래스애선 간단한 소수의 포멧이나, 문자열을 정렬하기 위한 메소드를 추가 할 예정입니다.

---
+++ src/main/java/me/else_junsuk/junseclibrary/stringsapi/C.java
@@ -0,0 +1,11 @@
+package me.else_junsuk.junseclibrary.stringsapi;
+
+import java.text.DecimalFormat;
+
+public class C {
+
+    /**
+     * Double형태의 소수를 #.##으로 포멧합니다. ex) 1.23456... -> 1.23
+     */
+    public static DecimalFormat doubleFormatter = new DecimalFormat("#.##");
+}
