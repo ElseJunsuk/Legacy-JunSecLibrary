4c63b6f79e1233d209424fe5046658137b645e19
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 18:33:56 +0900

    C클래스에 소숫점 3번쨰 자리까지 포멧이 가능한 메소드를 생성했습니다.

--- src/main/java/me/else_junsuk/junseclibrary/stringsapi/C.java
+++ src/main/java/me/else_junsuk/junseclibrary/stringsapi/C.java
@@ -8,4 +8,13 @@ public class C {
      * Double형태의 소수를 #.##으로 포멧합니다. ex) 1.23456... -> 1.23
      */
     public static DecimalFormat doubleFormatter = new DecimalFormat("#.##");
+    public static DecimalFormat doubleP3Formatter = new DecimalFormat("#.###");
+
+    public static Double doubleFormat(double doub) {
+        return Double.parseDouble(doubleFormatter.format(doub));
+    }
+
+    public static Double doubleP3Format(double doub) {
+        return Double.parseDouble(doubleP3Formatter.format(doub));
+    }
 }
