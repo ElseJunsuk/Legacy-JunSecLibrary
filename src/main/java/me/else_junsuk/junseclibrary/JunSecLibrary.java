16aa4cd336032ea03d8727f89e04d12656729f82
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 02:33:36 +0900

    라이브러리의 액세스 오류를 해결했습니다.

--- src/main/java/me/else_junsuk/junseclibrary/JunSecLibrary.java
+++ src/main/java/me/else_junsuk/junseclibrary/JunSecLibrary.java
@@ -1,22 +1,21 @@
 package me.else_junsuk.junseclibrary;
 
-import me.else_junsuk.junseclibrary.localelib.LocaleManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JunSecLibrary {
 
     private final JavaPlugin plugin;
-    private final LocaleManager localeManager;
 
     public JunSecLibrary(JavaPlugin plugin) {
         this.plugin = plugin;
-        this.localeManager = new LocaleManager();
+    }
+
+    public boolean isEnable() {
+        return this != null;
     }
 
     public JavaPlugin getJunSecLibrary() {
         return plugin;
     }
-
-    public LocaleManager getLocaleManager() { return localeManager; }
 
 }
